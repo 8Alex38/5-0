67d87efccf7124e7d8a4d8d7c80101b42a9448a8
8Alex38 <se64119@gmail.com>
2024-10-04 14:59:03 +0300

    second test

--- UnitTest2/UnitTest2.cpp
+++ UnitTest2/UnitTest2.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../5.0/5.0.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +9,12 @@ namespace UnitTest2
 	TEST_CLASS(UnitTest2)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
 		}
 	};
 }
